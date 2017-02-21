597ba74a7bbf9b9edbfefda57833d8ccac4373ab
Liang Cheng <liangcheng6@gmail.com>
2017-02-21 06:14:09 -0500

    first version of gameLevel code.

--- assets/javascript/gameLevel.js
+++ assets/javascript/gameLevel.js
@@ -1,16 +1,47 @@
 var gameLevel = function(answerString, cardImg, musicTrack) {
 
 	var private = {
-		answerString: answerString,
+		answerString: answerString.toLowerCase(),
 		cardImg: cardImg,
 		musicTrack: musicTrack
 	}
 
 	return {
+		//a function that returns the array just like a hangman would. 
+		//for example, "_ _ A _ _ _" if the user correctly guessed an A. 
 		guessResult: function (guesses) {
 			//guesses is an string of characters that user has put in to guess 
 			//toward answerString
+			answerCharArray = answerString.split('');
+			gussesCharArray = guesses.toLowerCase().split('');
+			resultCharArray = []
+			isMatched = true;
+			for (i = 0; i< answerString i++) {
+				answerChar = answerCharArray[i];
+				if (_.findInex(guessesCharArray, answerChar) != -1) {
+					//GuessChar is within the answerCharArray
+					resultCharArray.push(answerChar);
+				}else {
+					resultCharArray.push('_');
+					isMatched = false;
+				}
+			}
+
+			result = {
+				resultArray: resultCharArray
+				isMatched: isMatched
+			}
+			return resultCharArray;
 		}
+
+		getMusicTrack() {
+			return private["musicTrack"];
+		}
+		getCardImg() {
+			return private["cardImg"];
+		}
+
+
 	}
 
-}
+};
