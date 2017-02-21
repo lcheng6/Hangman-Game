8660415d5486dbe5e1c2551ffc803e72be7085f9
Liang Cheng <liangcheng6@gmail.com>
2017-02-20 19:40:49 -0500

    added lodash, a few assets, and basic layout of the page.

---
+++ javascript/gameLevel.js
@@ -0,0 +1,16 @@
+var gameLevel = function(answerString, cardImg, musicTrack) {
+
+	var private = {
+		answerString: answerString,
+		cardImg: cardImg,
+		musicTrack: musicTrack
+	}
+
+	return {
+		guessResult: function (guesses) {
+			//guesses is an string of characters that user has put in to guess 
+			//toward answerString
+		}
+	}
+
+}
